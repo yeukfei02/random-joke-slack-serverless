4bef8cacc275224353cf1b854ac7b86eb41cd24b
Donald Wu <yeukfei02@gmail.com>
2020-10-14 22:24:25 +0800

    feat: 🎸 add sendMessageToSlack func

--- src/getJoke/handler.ts
+++ src/getJoke/handler.ts
@@ -1,22 +1,29 @@
 import { Handler } from 'aws-lambda';
 import axios from 'axios';
 
+import env from 'dotenv';
+env.config();
+
+import { Joke } from '../../interface/Joke';
+
 export const getJoke: Handler = async (event: any) => {
-  const jokeResult = await getJokeRequest();
-  console.log('jokeResult = ', jokeResult);
+  const jokeResult: Joke = await getJokeRequest();
+  if (jokeResult) {
+    const message = `Setup: ${jokeResult.setup} Punchline: ${jokeResult.punchline}`;
+    await sendMessageToSlack(message);
+  }
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'getJoke',
-      jokeResult: jokeResult,
     }),
   };
   return response;
 };
 
 async function getJokeRequest() {
-  let result = null;
+  let result: any = {};
 
   const response = await axios.get(`https://official-joke-api.appspot.com/jokes/random`);
   if (response) {
@@ -27,4 +34,24 @@ async function getJokeRequest() {
   }
 
   return result;
+}
+
+async function sendMessageToSlack(message: string) {
+  const response = await axios.post(
+    `https://slack.com/api/chat.postMessage`,
+    {
+      token: process.env.SLACK_OAUTH_TOKEN,
+      channel: 'general',
+      text: message,
+    },
+    {
+      headers: {
+        'Content-type': 'application/json',
+      },
+    },
+  );
+  if (response) {
+    const responseData = response.data;
+    console.log('responseData = ', responseData);
+  }
 }
