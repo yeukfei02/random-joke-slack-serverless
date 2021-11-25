8a40a41389a71eea489dbfa8237167240c2982b5
Donald Wu <yeukfei02@gmail.com>
2021-11-25 22:57:22 +0800

    fix: 🐛 rename to helpers and use data as file

--- src/getJoke/handler.ts
+++ src/getJoke/handler.ts
@@ -1,21 +1,22 @@
 // import env from 'dotenv';
 // env.config();
 
-import { APIGatewayEvent, Context, Callback, Handler } from 'aws-lambda';
+import { APIGatewayEvent, Context, Handler } from 'aws-lambda';
 import awsXRay from 'aws-xray-sdk';
 import awsSdk from 'aws-sdk';
 if (process.env._X_AMZN_TRACE_ID) {
   awsXRay.captureAWS(awsSdk);
 }
 
-import axios from 'axios';
 import _ from 'lodash';
 
-import { sendMessageToSlackChannel } from '../../common/common';
+import { sendMessageToSlackChannel } from '../../helpers/helpers';
 import { Joke } from '../../interface/Joke';
+import { data } from '../../data/data';
 
-export const getJoke: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
-  const jokeResult: Joke = await getJokeRequest();
+export const getJoke: Handler = async (event: APIGatewayEvent, context: Context) => {
+  const jokeResultList: Joke[] = data;
+  const jokeResult = _.sample(jokeResultList) as Joke;
   console.log('jokeResult = ', jokeResult);
 
   if (!_.isEmpty(jokeResult)) {
@@ -31,17 +32,3 @@ export const getJoke: Handler = async (event: APIGatewayEvent, context: Context,
   };
   return response;
 };
-
-async function getJokeRequest() {
-  let result: any = {};
-
-  const response = await axios.get(`https://official-joke-api.appspot.com/jokes/random`);
-  if (response) {
-    const responseData = response.data;
-    if (responseData) {
-      result = responseData;
-    }
-  }
-
-  return result;
-}
