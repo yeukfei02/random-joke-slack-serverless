9c27b763adead0fbe8c3080409d6de37634faca5
Donald Wu <yeukfei02@gmail.com>
2022-03-14 17:31:10 +0800

    fix: 🐛 remove callback and update sls lib

--- src/getMain/handler.ts
+++ src/getMain/handler.ts
@@ -1,11 +1,11 @@
-import { APIGatewayEvent, Context, Callback, Handler } from 'aws-lambda';
+import { APIGatewayEvent, Context, Handler } from 'aws-lambda';
 import awsXRay from 'aws-xray-sdk';
 import awsSdk from 'aws-sdk';
 if (process.env._X_AMZN_TRACE_ID) {
   awsXRay.captureAWS(awsSdk);
 }
 
-export const getMain: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
+export const getMain: Handler = async (event: APIGatewayEvent, context: Context) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
